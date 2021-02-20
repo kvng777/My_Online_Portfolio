0dca691a6787fdc091ec7e6e218543afbfb22049
kvng777 <kjangamboa@hotmail.com>
2021-02-20 15:12:02 +0800

    Removed GIF files

--- app.js
+++ app.js
@@ -109,192 +109,192 @@ const wechat =  `
 //                 </div>
 // `;
 
-const mdContent= `
-<div class="container">
-<h1 class="mb-3 mt-5 text-center">Portfolio</h1>
-<div class="row">
+// const mdContent= `
+// <div class="container">
+// <h1 class="mb-3 mt-5 text-center">Portfolio</h1>
+// <div class="row">
 
-<div class="col-sm-12 col-md-6 col-lg-4">
-    <div class="card project mx-auto">
-        <img src="/images/proj3.gif" alt="Link to project 1">
-            <div class="image-overlay">
-                <div class="desc container">
-                    <p class="fw-light">A mobile <strong>responsive</strong> form by using the <strong>flex box</strong> layout which allows user to insert information with all sorts of <strong>input</strong> type.</p>
+// <div class="col-sm-12 col-md-6 col-lg-4">
+//     <div class="card project mx-auto">
+//         <img src="/images/proj3.png" alt="Link to project 1">
+//             <div class="image-overlay">
+//                 <div class="desc container">
+//                     <p class="fw-light">A mobile <strong>responsive</strong> form by using the <strong>flex box</strong> layout which allows user to insert information with all sorts of <strong>input</strong> type.</p>
 
-                    <h6>Tech Stack</h6>
-                        <ul class="list-unstyled d-flex mt-1 justify-content-center">
-                            <li class="mx-2">
-                            <img class="" src="images/html.png" alt="">
-                            </li>
+//                     <h6>Tech Stack</h6>
+//                         <ul class="list-unstyled d-flex mt-1 justify-content-center">
+//                             <li class="mx-2">
+//                             <img class="" src="images/html.png" alt="">
+//                             </li>
 
-                            <li class="mx-2">
-                            <img class="" src="images/css.png" alt="">
-                            </li>
-                        </ul>
-                        <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/3_Online_Registration_Form/" target="_blank">Visit Site</a>
-                </div>
-            </div>
-        <div class="card-body">
-            <h6 class="card-title my-auto">Registration Form</h6>
-            <p class="card-text"></p>
-        </div>
-    </div>
-</div>
+//                             <li class="mx-2">
+//                             <img class="" src="images/css.png" alt="">
+//                             </li>
+//                         </ul>
+//                         <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/3_Online_Registration_Form/" target="_blank">Visit Site</a>
+//                 </div>
+//             </div>
+//         <div class="card-body">
+//             <h6 class="card-title my-auto">Registration Form</h6>
+//             <p class="card-text"></p>
+//         </div>
+//     </div>
+// </div>
 
-<div class="col-sm-12 col-md-6 col-lg-4">
-    <div class="card project mx-auto">
-        <img src="images/proj4.gif" alt="Link to project 2">
-        <div class="image-overlay">
-            <div class="desc container">
-                <p class="fw-light">A mobile <strong>responsive</strong> web-app style using partials in keeping codes organized. Used <strong>Sass syntaxt</strong> keeping stylesheet neat and easy to read.</p>
+// <div class="col-sm-12 col-md-6 col-lg-4">
+//     <div class="card project mx-auto">
+//         <img src="images/proj4.png" alt="Link to project 2">
+//         <div class="image-overlay">
+//             <div class="desc container">
+//                 <p class="fw-light">A mobile <strong>responsive</strong> web-app style using partials in keeping codes organized. Used <strong>Sass syntaxt</strong> keeping stylesheet neat and easy to read.</p>
 
-                <h6>Tech Stack</h6>
-                    <ul class="list-unstyled d-flex mt-1 justify-content-center">
-                        <li class="mx-2">
-                        <img class="" src="images/html.png" alt="">
-                        </li>
+//                 <h6>Tech Stack</h6>
+//                     <ul class="list-unstyled d-flex mt-1 justify-content-center">
+//                         <li class="mx-2">
+//                         <img class="" src="images/html.png" alt="">
+//                         </li>
 
-                        <li class="mx-2">
-                        <img class="" src="images/sass.png" alt="">
-                        </li>
-                    </ul>
-                    <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/4_style_guide/" target="_blank">Visit Site</a>
-            </div>
-        </div>
-        <div class="card-body">
-            <h6 class="card-title my-auto">Style Guide</h6>
-            <p class="card-text"></p>
-        </div>
-    </div>
-</div>
+//                         <li class="mx-2">
+//                         <img class="" src="images/sass.png" alt="">
+//                         </li>
+//                     </ul>
+//                     <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/4_style_guide/" target="_blank">Visit Site</a>
+//             </div>
+//         </div>
+//         <div class="card-body">
+//             <h6 class="card-title my-auto">Style Guide</h6>
+//             <p class="card-text"></p>
+//         </div>
+//     </div>
+// </div>
 
-<div class="col-sm-12 col-md-6 col-lg-4">
-    <div class="card project mx-auto">
-        <img src="images/proj5.gif" alt="Link to project 3">
-        <div class="image-overlay">
-            <div class="desc container"> 
-                <p class="fw-light">An <strong>interactive</strong> photo gallery with using a baggutebox.js <strong>lightbox</strong> feature for viewing large size photos and with a <strong>searchFilter.js</strong> to search for particular image.</p>
+// <div class="col-sm-12 col-md-6 col-lg-4">
+//     <div class="card project mx-auto">
+//         <img src="images/proj5.png" alt="Link to project 3">
+//         <div class="image-overlay">
+//             <div class="desc container"> 
+//                 <p class="fw-light">An <strong>interactive</strong> photo gallery with using a baggutebox.js <strong>lightbox</strong> feature for viewing large size photos and with a <strong>searchFilter.js</strong> to search for particular image.</p>
 
-                <h6>Tech Stack</h6>
-                    <ul class="list-unstyled d-flex mt-1 justify-content-center">
-                        <li class="mx-2">
-                        <img class="" src="images/html.png" alt="">
-                        </li>
+//                 <h6>Tech Stack</h6>
+//                     <ul class="list-unstyled d-flex mt-1 justify-content-center">
+//                         <li class="mx-2">
+//                         <img class="" src="images/html.png" alt="">
+//                         </li>
 
-                        <li class="mx-2">
-                        <img class="" src="images/sass.png" alt="">
-                        </li>
+//                         <li class="mx-2">
+//                         <img class="" src="images/sass.png" alt="">
+//                         </li>
 
-                        <li class="mx-2">
-                        <img class="" src="images/js.png" alt="">
-                        </li>
-                    </ul>
-                    <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/5_photo_gallery/" target="_blank">Visit Site</a>
-            </div>
-        </div>
-        <div class="card-body">
-            <h6 class="card-title my-auto">Interactive Photo Gallery</h6>
-            <p class="card-text"></p>
-        </div>
-    </div>
-</div>
+//                         <li class="mx-2">
+//                         <img class="" src="images/js.png" alt="">
+//                         </li>
+//                     </ul>
+//                     <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/5_photo_gallery/" target="_blank">Visit Site</a>
+//             </div>
+//         </div>
+//         <div class="card-body">
+//             <h6 class="card-title my-auto">Interactive Photo Gallery</h6>
+//             <p class="card-text"></p>
+//         </div>
+//     </div>
+// </div>
 
-<div class="col-sm-12 col-md-6 col-lg-4">
-    <div class="card project mx-auto">
-        <img src="images/proj6.gif" alt="Link to project 4">
-        <div class="image-overlay">
-            <div class="desc container">
-                <p class="fw-light">A game show web app thats built with <strong>arrays</strong> and <strong>JS</strong> functions to check against selected letters which in the end gives you and win or lose.</p>
+// <div class="col-sm-12 col-md-6 col-lg-4">
+//     <div class="card project mx-auto">
+//         <img src="images/proj6.png" alt="Link to project 4">
+//         <div class="image-overlay">
+//             <div class="desc container">
+//                 <p class="fw-light">A game show web app thats built with <strong>arrays</strong> and <strong>JS</strong> functions to check against selected letters which in the end gives you and win or lose.</p>
 
-                <h6>Tech Stack</h6>
-                    <ul class="list-unstyled d-flex mt-1 justify-content-center">
-                        <li class="mx-2">
-                        <img class="" src="images/html.png" alt="">
-                        </li>
+//                 <h6>Tech Stack</h6>
+//                     <ul class="list-unstyled d-flex mt-1 justify-content-center">
+//                         <li class="mx-2">
+//                         <img class="" src="images/html.png" alt="">
+//                         </li>
 
-                        <li class="mx-2">
-                        <img class="" src="images/sass.png" alt="">
-                        </li>
+//                         <li class="mx-2">
+//                         <img class="" src="images/sass.png" alt="">
+//                         </li>
 
-                        <li class="mx-2">
-                        <img class="" src="images/js.png" alt="">
-                        </li>
-                    </ul>
-                    <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/6-game_show_app_v1.2/" target="_blank">Visit Site</a>
-            </div>
-        </div>
+//                         <li class="mx-2">
+//                         <img class="" src="images/js.png" alt="">
+//                         </li>
+//                     </ul>
+//                     <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/6-game_show_app_v1.2/" target="_blank">Visit Site</a>
+//             </div>
+//         </div>
 
-        <div class="card-body">
-            <h6 class="card-title my-auto">Game Show App</h6>
-            <p class="card-text"></p>
-        </div>
-    </div>
-</div>
+//         <div class="card-body">
+//             <h6 class="card-title my-auto">Game Show App</h6>
+//             <p class="card-text"></p>
+//         </div>
+//     </div>
+// </div>
 
-<div class="col-sm-12 col-md-6 col-lg-4">
-    <div class="card project mx-auto">
-        <img src="images/proj7.gif" alt="Link to project 5">
-        <div class="image-overlay">
-            <div class="desc container"> 
-                <p class="fw-light">A web responsive web app built with using <strong>CSS grid</strong>. Interactive data charts and graphs are being populated with using <strong>Chart.js</strong> and <strong>Object literals</strong>.</p>
+// <div class="col-sm-12 col-md-6 col-lg-4">
+//     <div class="card project mx-auto">
+//         <img src="images/proj7.png" alt="Link to project 5">
+//         <div class="image-overlay">
+//             <div class="desc container"> 
+//                 <p class="fw-light">A web responsive web app built with using <strong>CSS grid</strong>. Interactive data charts and graphs are being populated with using <strong>Chart.js</strong> and <strong>Object literals</strong>.</p>
 
-                <h6>Tech Stack</h6>
-                    <ul class="list-unstyled d-flex mt-1 justify-content-center">
-                        <li class="mx-2">
-                        <img class="" src="images/html.png" alt="">
-                        </li>
+//                 <h6>Tech Stack</h6>
+//                     <ul class="list-unstyled d-flex mt-1 justify-content-center">
+//                         <li class="mx-2">
+//                         <img class="" src="images/html.png" alt="">
+//                         </li>
 
-                        <li class="mx-2">
-                        <img class="" src="images/sass.png" alt="">
-                        </li>
+//                         <li class="mx-2">
+//                         <img class="" src="images/sass.png" alt="">
+//                         </li>
 
-                        <li class="mx-2">
-                        <img class="" src="images/js.png" alt="">
-                        </li>
-                    </ul>
-                    <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/7-web_app_dashboard_v3.1/" target="_blank">Visit Site</a>
-            </div>
-        </div>
-        <div class="card-body">
-            <h6 class="card-title my-auto">Web App Dashboard</h6>
-            <p class="card-text"></p>
-        </div>
-    </div>
-</div>
+//                         <li class="mx-2">
+//                         <img class="" src="images/js.png" alt="">
+//                         </li>
+//                     </ul>
+//                     <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/7-web_app_dashboard_v3.1/" target="_blank">Visit Site</a>
+//             </div>
+//         </div>
+//         <div class="card-body">
+//             <h6 class="card-title my-auto">Web App Dashboard</h6>
+//             <p class="card-text"></p>
+//         </div>
+//     </div>
+// </div>
 
-<div class="col-sm-12 col-md-6 col-lg-4">
-    <div class="card project mx-auto">
-        <img src="images/proj8.gif" alt="Link to project 6">
-        <div class="image-overlay">
-            <div class="desc container">
-                <p class="fw-light">A web responsive Employee Directory populating employee data with using <strong>fetch API</strong>. Built with modal pop-up feature to view through employee data.</p>
+// <div class="col-sm-12 col-md-6 col-lg-4">
+//     <div class="card project mx-auto">
+//         <img src="images/proj8.png" alt="Link to project 6">
+//         <div class="image-overlay">
+//             <div class="desc container">
+//                 <p class="fw-light">A web responsive Employee Directory populating employee data with using <strong>fetch API</strong>. Built with modal pop-up feature to view through employee data.</p>
 
-                <h6>Tech Stack</h6>
-                    <ul class="list-unstyled d-flex mt-1 justify-content-center">
-                        <li class="mx-2">
-                        <img class="" src="images/html.png" alt="">
-                        </li>
+//                 <h6>Tech Stack</h6>
+//                     <ul class="list-unstyled d-flex mt-1 justify-content-center">
+//                         <li class="mx-2">
+//                         <img class="" src="images/html.png" alt="">
+//                         </li>
 
-                        <li class="mx-2">
-                        <img class="" src="images/sass.png" alt="">
-                        </li>
+//                         <li class="mx-2">
+//                         <img class="" src="images/sass.png" alt="">
+//                         </li>
 
-                        <li class="mx-2">
-                        <img class="" src="images/js.png" alt="">
-                        </li>
-                    </ul>
-                    <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/8_employee_directory_v1/" target="_blank">Visit Site</a>
-            </div>
-        </div>
-        <div class="card-body">
-            <h6 class="card-title my-auto">Employee Directory</h6>
-            <p class="card-text"></p>
-        </div>
-    </div>
-</div>
-</div> 
-</div>
-`;
+//                         <li class="mx-2">
+//                         <img class="" src="images/js.png" alt="">
+//                         </li>
+//                     </ul>
+//                     <a class="btn bg-info fs-6 mx-auto w-75" href="https://kvng777.github.io/8_employee_directory_v1/" target="_blank">Visit Site</a>
+//             </div>
+//         </div>
+//         <div class="card-body">
+//             <h6 class="card-title my-auto">Employee Directory</h6>
+//             <p class="card-text"></p>
+//         </div>
+//     </div>
+// </div>
+// </div> 
+// </div>
+// `;
 
 //wechat qrcode pop-over
 $(document).ready(function(){
